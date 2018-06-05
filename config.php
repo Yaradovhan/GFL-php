0707db1ae1249ef4f88b9a70a3923ac672554294
Yaradovhan <yara.dovhan@gmail.com>
2018-06-05 14:32:08 +0300

    app dir

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-define("ROOT_DIR","/home/user94/www/GFL");
+define("ROOT_DIR","/home/user94/www/GFL-php");
 define("UPDIR", ROOT_DIR."/upDir");
 define("UPERROR","Permission denied or incorrect data");
 define("SUCCSESS_UPLOAD", "Very good. File was Uploaded");
